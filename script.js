9ac8ec6ed3e4c675d9fccb78c522f912c136e681
pizzaman20000 <pizzaman20000@users.noreply.github.com>
2021-02-09 20:33:08 +1300

    updated results to show group1 or 2

--- script.js
+++ script.js
@@ -2,27 +2,39 @@ const modalWrapper = document.querySelector('.modal-wrapper');
 const tableUsers = document.querySelector('.table-users');
 let id;
 
+function get(name){
+  if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+     return decodeURIComponent(name[1]);
+}
+
 // Create element and render users
 const renderUser = (doc, index) => {
   const tr = `
     <tr data-id='${doc.id}'>
       <td>${index}</td>
-      <td>${doc.data().firstName}</td>
-      <td>${doc.data().firstTest}</td>
-      <td>${doc.data().secondTest}</td>
+      <td>${doc.data().name}</td>
+      <td>${doc.data().time}</td>
     </tr>
   `;
   tableUsers.insertAdjacentHTML('beforeend', tr);
 }
 
-// Real time listener
-db.collection('users').onSnapshot(snapshot => {
-  var index = 1;
-  snapshot.docChanges().forEach(change => {
-    if(change.type === 'added') {
-      renderUser(change.doc, index);
-      index++;
-    }
-  })
-})
+function initDatabase()
+{
+  let test_name = get('test_name');
+  console.log(test_name);
+  if (test_name==undefined)
+    test_name = 'group1';
 
+  db.collection(test_name).onSnapshot(snapshot => {
+    var index = 1;
+    snapshot.docChanges().forEach(change => {
+      if(change.type === 'added') {
+        renderUser(change.doc, index);
+        index++;
+      }
+    })
+  })
+}
+
+initDatabase();
